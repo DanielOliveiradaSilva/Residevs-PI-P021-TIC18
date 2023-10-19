13fc0b42b077859b36b44b055ff662cbfd4ac80d
daniel <danieloliveira.s558@gmail.com>
2023-10-19 09:49:05 -0300

    modificando caminho do arquivo

--- main.cpp
+++ main.cpp
@@ -1,22 +1,20 @@
 #include "BD.h"
-
+#include <filesystem>
 void ListarUsuario(vector<Usuario> usuarios);
 void ListarLivros(vector<Livro> livros);
 
-
-
-
 int main()
 {
-    vector<Usuario> dados_carregados = BD::CarregarDadosDeUsuarios("/home/daniel/Área de Trabalho/Biblioteca/BD/usuarios.txt");
+
+
+
+    vector<Usuario> dados_carregados = BD::CarregarDadosDeUsuarios("../BD/usuarios.txt");
     BD::ListarUsuario(dados_carregados);
-   
-    vector<Livro> dados_de_livros = BD::CarregarDadosDeLivros("/home/daniel/Área de Trabalho/Biblioteca/BD/livros.txt");
+
+    vector<Livro> dados_de_livros = BD::CarregarDadosDeLivros("../BD/livros.txt");
     BD::ListarLivros(dados_de_livros);
 
-    
-   vector<Emprestimo> dados_de_emprestimos = BD::CarregarDadosDeEmprestimos("/home/daniel/Área de Trabalho/Biblioteca/BD/emprestimos.txt");
-   BD::ListarEmprestimos(dados_de_emprestimos);
-   return 0;
- 
+    vector<Emprestimo> dados_de_emprestimos = BD::CarregarDadosDeEmprestimos("../BD/emprestimos.txt");
+    BD::ListarEmprestimos(dados_de_emprestimos);
+    return 0;
 }
